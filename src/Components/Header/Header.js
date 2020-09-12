0d563014c77e272a14ef8f37e95ce5d1504af155
nhokphal <phalnhok@gmail.com>
2020-09-12 12:05:35 +0700

    1 file modified, 1 added, 2 deleted

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '../../Components/Header.css';
-import logoUrl from '../../assets/logo.svg';
-
+import logoUrl from 'src/Assets/logo.svg';
 
 const Header = () => (
   <div className="header">
-    <img src={ "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"} alt="Tesla" />
+    <img src={logoUrl} alt="Tesla" />
   </div>
 )
 export default Header;
