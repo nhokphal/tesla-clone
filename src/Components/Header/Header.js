14c1b859e7ae5f1d4c3f546fbf009ee951b46179
nhokphal <phalnhok@gmail.com>
2020-09-12 11:39:57 +0700

    1 file modified

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ import logoUrl from '../../assets/logo.svg';
 
 const Header = () => (
   <div className="header">
-    <img src={logoUrl} alt="Tesla" />
+    <img src={ "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"} alt="Tesla" />
   </div>
 )
 export default Header;
